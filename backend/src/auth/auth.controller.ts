6340bcb18c2b45217e598fdf547c4ccd33a43631
UnDeadCharger <rexfury121@gmail.com>
2023-11-11 15:58:02 +0700

    add Hashing

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { Tokens } from './types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('local/signup')
-  signupLocal(@Body() dto: AuthDto) {
+  signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
     this.authService.signupLocal(dto);
   }
   @Post('local/signin')
