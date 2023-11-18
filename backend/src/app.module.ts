5d733d19d3c7c0f656fb8999ff15d55b51212bea
Ha Tuan Lam <tuanlam16102002@gmail.com>
2023-11-18 16:41:03 +0700

    remove vercel

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,31 +1,33 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Median')
-  //   .setDescription('The Median API description')
-  //   .setVersion('0.1')
-  //   .addBearerAuth(
-  //     {
-  //       type: 'http',
-  //       scheme: 'bearer',
-  //       bearerFormat: 'JWT',
-  //       name: 'jwt',
-  //       description: 'Enter JWT token',
-  //       in: 'header',
-  //     },
-  //     'jwt', // This name should match the security name defined below
-  //   )
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'jwt',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'jwt', // This name should match the security name defined below
+    )
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
 bootstrap();
