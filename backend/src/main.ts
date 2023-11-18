41aa289ac743485ef202a2d6130d184e9b913558
Ha Tuan Lam <tuanlam16102002@gmail.com>
2023-11-18 16:10:06 +0700

    remove prisma module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,27 +7,27 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Median')
-  //   .setDescription('The Median API description')
-  //   .setVersion('0.1')
-  //   .addBearerAuth(
-  //     {
-  //       type: 'http',
-  //       scheme: 'bearer',
-  //       bearerFormat: 'JWT',
-  //       name: 'jwt',
-  //       description: 'Enter JWT token',
-  //       in: 'header',
-  //     },
-  //     'jwt', // This name should match the security name defined below
-  //   )
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'jwt',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'jwt', // This name should match the security name defined below
+    )
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
 bootstrap();
