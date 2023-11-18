39f3ae017896fefbc329f05e99e791ac9b1be5c8
Ha Tuan Lam <tuanlam16102002@gmail.com>
2023-11-18 15:37:10 +0700

    remove validationpipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(4000);
 }
