251c7c31e72f1e09cc613805cf1f66aef3e76f20
UnDeadCharger <rexfury121@gmail.com>
2023-12-02 13:20:45 +0700

    AWP-24: resolve merge conflicts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
@@ -10,6 +9,7 @@ import { EmailConfirmationController } from './email-confirmation/email-confirma
 import { EmailConfirmationService } from './email-confirmation/email-confirmation.service';
 import { EmailConfirmationModule } from './email-confirmation/email-confirmation.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
