63a564146287837397c33f7c26b101d8820466a2
NguyenQuang179 <79895324+NguyenQuang179@users.noreply.github.com>
2023-12-03 23:01:06 +0700

    AWP-80 feature: Activate account with email when sign in

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,4 +1,4 @@
-import { ArrowRightOutlined, FacebookOutlined, GoogleOutlined, HomeOutlined } from '@ant-design/icons';
+import { ArrowRightOutlined, FacebookOutlined, GoogleOutlined, HomeOutlined, MailOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input, notification } from 'antd';
 import useForm from 'antd/es/form/hooks/useForm';
 import axios from 'axios';
@@ -18,6 +18,17 @@ export default function SignInPage() {
   const [form] = useForm();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const activateAccount = async (values: { email: string | undefined }) => {
+    try {
+      await axios.post(
+        `${import.meta.env.VITE_REACT_APP_SERVER_URL}/email-confirmation/resendVerification`,
+        values,
+      )
+    } catch (err) {
+      console.log(err)
+    }
+  }
   const onFinish = async (values: FieldType) => {
     try {
       setIsSubmitting(true);
@@ -31,9 +42,14 @@ export default function SignInPage() {
       navigate('/home');
       setIsSubmitting(false);
       document.body.style.cursor = 'default';
-    } catch (err) {
-      console.log(err);
-      openNotification();
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      console.log(err.response.data.message);
+      if (err.response.data.statusCode == 401 && err.response.data.message == "Email not confirmed") {
+        await activateAccount({ email: values.email })
+        setCurrentStep(1)
+      }
+      else openNotification();
       setIsSubmitting(false);
       document.body.style.cursor = 'default';
     }
@@ -45,7 +61,6 @@ export default function SignInPage() {
   const [api, contextHolder] = notification.useNotification({
     stack: { threshold: 3 },
   });
-
   const openNotification = () => {
     api['error']({
       message: 'Sign In Failed',
@@ -55,27 +70,10 @@ export default function SignInPage() {
     });
   };
 
-  return (
-    <div className="h-screen w-screen flex justify-center items-center fixed">
-      {contextHolder}
-      {/* Content */}
-      <div className="flex flex-row w-7/12 h-2/3 rounded-xl bg-white border-2 border-gray-300 shadow-xl overflow-hidden">
-        {/* Left Section */}
-        <div className="w-2/5 flex flex-col justify-start items-center bg-indigo-400 text-white">
-          <div className="w-full flex justify-start my-4">
-            <Link to={'/'}>
-              <Button className="ms-4 flex justify-center items-center border-white text-indigo-500 bg-white">
-                <HomeOutlined />
-              </Button>
-            </Link>
-          </div>
-          <h1 className="uppercase font-semibold text-xl text-center">
-            HQL Application
-          </h1>
-          <p className=" mt-2 mb-4 text-sm text-center">Welcome to HQL</p>
-          <img src={loginImg} className="object-contain box-border px-8" />
-        </div>
-        {/* Right Section */}
+  const steps = [
+    {
+      title: 'Fill In Information',
+      content: (
         <div className="flex flex-col flex-1 justify-center items-center overflow-auto">
           {/* {isSubmitting && <Spin />} */}
           <h1 className="uppercase font-semibold text-2xl text-center my-4">
@@ -169,7 +167,58 @@ export default function SignInPage() {
               Sign up
             </Link>
           </p>
+        </div>),
+    },
+    {
+      title: 'Email Verification',
+      content: (
+        <div className="flex flex-col flex-1 justify-center items-center overflow-auto">
+          <div className='w-2/3'>
+            <h1 className="uppercase font-semibold text-xl text-center my-4">
+              Activate Account
+            </h1>
+            <div>
+              <MailOutlined className='text-4xl' />
+            </div>
+            <div className='my-4 w-full overflow-hidden flex flex-col gap-4 text-gray-700 text-sm'>
+              <p>We've send a mail to</p>
+              <p className='font-semibold text-lg'>{form.getFieldValue('email') != "" ? form.getFieldValue('email') : "example@email.com"}</p>
+              <div>
+                <p >Please click the link in your email to activate your account. The link in the mail will expire in 5 minutes.</p>
+                <p> Don't forget to check your spam section!</p>
+              </div>
+              <p className=''>Didn't receive a mail? <span className='underline text-indigo-500 font-semibold hover:cursor-pointer'>Resend</span></p>
+            </div>
+          </div>
         </div>
+
+      ),
+    },
+  ];
+  const [currentStep, setCurrentStep] = useState(0);
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center fixed">
+      {contextHolder}
+      {/* Content */}
+      <div className="flex flex-row w-7/12 h-2/3 rounded-xl bg-white border-2 border-gray-300 shadow-xl overflow-hidden">
+        {/* Left Section */}
+        <div className="w-2/5 flex flex-col justify-start items-center bg-indigo-400 text-white">
+          <div className="w-full flex justify-start my-4">
+            <Link to={'/'}>
+              <Button className="ms-4 flex justify-center items-center border-white text-indigo-500 bg-white">
+                <HomeOutlined />
+              </Button>
+            </Link>
+          </div>
+          <h1 className="uppercase font-semibold text-xl text-center">
+            HQL Application
+          </h1>
+          <p className=" mt-2 mb-4 text-sm text-center">Welcome to HQL</p>
+          <img src={loginImg} className="object-contain box-border px-8" />
+        </div>
+        {/* Right Section */}
+        {steps[currentStep].content}
       </div>
       <img
         src={wave}
