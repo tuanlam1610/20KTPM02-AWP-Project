4d75e7876d9777233391ff3811d607224fc5b8e7
UnDeadCharger <rexfury121@gmail.com>
2023-12-27 01:01:10 +0700

    AWP-88 feat: added students api

--- backend/src/students/students.service.ts
+++ backend/src/students/students.service.ts
@@ -1,11 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateStudentDto } from './dto/create-students.dto';
 import { UpdateStudentDto } from './dto/update-students.dto';
+import { PopulateClassDto } from 'src/classes/dto/class-populate.dto';
 
 @Injectable()
 export class StudentsService {
   constructor(private prisma: PrismaService) {}
+  async joinClassByCode(classCode: string, studentId: string) {
+    const classToJoin = await this.prisma.class.findUnique({
+      where: { code: classCode },
+    });
+    if (!classToJoin) {
+      //return error: class not found
+      return HttpStatus.NOT_FOUND;
+    }
+    return this.prisma.student.update({
+      where: { id: studentId },
+      data: {
+        classMember: {
+          create: { class: { connect: { id: classToJoin.id } } },
+        },
+      },
+    });
+  }
+
+  async getAllClassesOfstudent(studentId: string) {
+    const classesOfStudent = await this.prisma.student.findUnique({
+      where: { id: studentId },
+      select: {
+        classMember: {
+          select: {
+            class: {
+              select: {
+                id: true,
+                name: true,
+                description: true,
+                code: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    return (
+      classesOfStudent?.classMember?.map((ct) => ({
+        id: ct.class.id,
+        name: ct.class.name,
+        description: ct.class.description,
+        code: ct.class.code,
+      })) || []
+    );
+  }
+  async populateStudents(studentsList: PopulateClassDto) {
+    const { students } = studentsList;
+
+    const studentsData = students.map((student) => ({
+      name: student.name,
+      id: student.studentId,
+    }));
+
+    const studentsCreated = await this.prisma.student.createMany({
+      data: studentsData,
+      // Set `skipDuplicates` to true if you want to skip inserting duplicate records
+      skipDuplicates: true,
+    });
+
+    return studentsCreated; // MB ignore the students that already exist
+  }
+
+  async mapStudentToUser(studentId: string, userId: string) {
+    try {
+      return this.prisma.student.update({
+        where: { id: studentId },
+        data: { userId: userId },
+      });
+    } catch (e) {
+      console.log(e, 'User is already mapped');
+    }
+  }
+
   //CRUD
   async create(createStudentDto: CreateStudentDto) {
     // Fetch StudentGrade by their IDs
