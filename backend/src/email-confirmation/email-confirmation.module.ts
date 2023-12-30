6082d6cab0036df529bf55b278000915fe799a20
UnDeadCharger <rexfury121@gmail.com>
2023-12-30 13:52:06 +0700

    AWP-88 feat: add invite by email for module

--- backend/src/email-confirmation/email-confirmation.module.ts
+++ backend/src/email-confirmation/email-confirmation.module.ts
@@ -1,34 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { EmailsModule } from 'src/emails/emails.module';
-import EmailsService from 'src/emails/emails.service';
 import { UsersModule } from 'src/users/users.module';
 import { EmailConfirmationController } from './email-confirmation.controller';
 import { EmailConfirmationService } from './email-confirmation.service';
 import { RtStrategy } from 'src/auth/strategies/rt.strategy';
 import { AtStrategy } from 'src/auth/strategies/at.strategy';
-import { AuthService } from 'src/auth/auth.service';
-import { AuthModule } from 'src/auth/auth.module';
 import { FirebaseModule } from 'src/firebase/firebase.module';
+import { ClassesModule } from 'src/classes/classes.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule,
     UsersModule,
+    ClassesModule,
     EmailsModule,
     AuthModule,
     JwtModule.register({}),
     FirebaseModule,
   ],
   controllers: [EmailConfirmationController],
-  providers: [
-    ConfigService,
-    AtStrategy,
-    RtStrategy,
-    EmailConfirmationService,
-    AuthService,
-  ],
+  providers: [ConfigService, AtStrategy, RtStrategy, EmailConfirmationService],
   exports: [EmailConfirmationService],
 })
 export class EmailConfirmationModule {}
