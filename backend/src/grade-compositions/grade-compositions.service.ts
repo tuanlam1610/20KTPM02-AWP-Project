88700d5dad0a2c295252820b0c55a7e23a297d6f
NguyenQuang179 <nnquanglop96dt@gmail.com>
2024-01-06 11:12:51 +0700

    AWP-86 chore: update order for grade board

--- backend/src/grade-compositions/grade-compositions.service.ts
+++ backend/src/grade-compositions/grade-compositions.service.ts
@@ -132,6 +132,9 @@ export class GradeCompositionsService {
             },
             grade: true,
           },
+          orderBy: {
+            studentId: 'asc',
+          },
         },
       },
     });
