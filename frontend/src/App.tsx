6f5b85513e6fc3ef656c23ec6ef5be41037b5e40
Ha Tuan Lam <tuanlam16102002@gmail.com>
2024-01-08 20:26:32 +0700

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -90,7 +90,7 @@ function App() {
           {/* Student Routes */}
           <Route
             path="/student"
-            element={<ProtectedRoute allowedRole={'student'} />}
+            // element={<ProtectedRoute allowedRole={'student'} />}
           >
             <Route path="home" element={<StudentHomePage />} />
             <Route path="class/:id" element={<StudentClassDetailsPage />} />
